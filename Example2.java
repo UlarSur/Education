2f5fc09e616c3eef55d3daaf2751f5abdcb98c73
Ular Sur <64522495+UlarSur@users.noreply.github.com>
2022-05-24 17:27:16 +0600

    Add files via upload
    Еще одна легкая программа. Знакомство с переменными.

---
+++ Example2.java
@@ -0,0 +1,19 @@
+/* 
+Еще одна легкая программа №2
+Естественно название класса как и название исходного файла "Example2.java"
+*/
+class Example2 {
+	public static void main (String args[]) {
+		int num; // в этой строке кода объявляется переменная с именем num
+		
+		num = 100; // в этой строке кода переменной num
+		           // присваивается значение 100
+		
+		System.out.println ("Это переменная num: " + num) ;
+		
+		num = num * 2;
+		
+		System.out.print("Значение переменной num * 2 равно ");
+		System.out.println(num) ;
+	}
+}
